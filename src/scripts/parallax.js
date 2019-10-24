f2820e41adf99fe17b4c382ee64dc4e6688f1981
andreyzavrichko <andrey@zavrichko.dev>
2019-10-24 18:50:42 +0300

    update parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -5,15 +5,13 @@ function moveLayersDependOnScroll(wScroll) {
     Array.from(layers).forEach(layer => {
         let divider = layer.dataset.speed;
         
-        if (divider == 0.2) {           
-            divider = 3;            
-        }
+      
 
         const strafe = wScroll * divider / 37;              
 
-        if (divider < 0.6 || divider >= 2) { 
+        
             layer.style.transform = `translateY(-${strafe}%)`;
-        }        
+               
     });    
 }
 
